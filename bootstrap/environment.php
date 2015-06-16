1df592fdf81e06bd804017e1bd5c60557f3f65c3
Sylvia Wei <trinary@gmail.com>
2015-06-16 12:56:29 -0400

    Minor clean up

--- bootstrap/environment.php
+++ bootstrap/environment.php
@@ -2,19 +2,7 @@
 |--------------------------------------------------------------------------
 | Detect The Application Environment
 |--------------------------------------------------------------------------
-|
-| Laravel takes a dead simple approach to your application environments
-| so you can just specify a machine name for the host that matches a
-| given environment, then we will automatically detect it for you.
-|
-| Based on 
-  - http://developers.ph/laravel-framework/laravel-5/how-to-setup-multiple-environment-for-laravel-5-developers-way/
-  - https://mattstauffer.co/blog/laravel-5.0-environment-detection-and-environment-variables
-  - 
-
-TODO: Add Automatic Environmental detection.
 */
-
 
 $env = $app->detectEnvironment(function(){
   //Load default configurations
